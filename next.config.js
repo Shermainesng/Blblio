8eb9df3cf12e3dea454f3908168dd632eaf89dcf
shermaine sng <shersmallsng@gmail.com>
2024-01-11 00:29:07 -0800

    update pw

--- next.config.js
+++ next.config.js
@@ -1,26 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const nextAuthUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://blblio-xhl7.vercel.app/api/auth"
+    : "http://localhost:3000/api/auth";
+
 const nextConfig = {
   async redirects() {
     return [
-               {
-                   source: '/',
-                   destination: '/books',
-                   permanent: true,
-                },
-           ]
-   },
-  reactStrictMode: false,
-  swcMinify:true,
-  images: {
-    domains:[
-      "books.google.com",
-      // "st4.depositphotos.com", 
-      "depositphotos.com",
-      "as1.ftcdn.net"
-    ]
+      {
+        source: "/",
+        destination: "/books",
+        permanent: true,
+      },
+    ];
   },
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [
+      "books.google.com",
+      // "st4.depositphotos.com",
+      "depositphotos.com",
+      "as1.ftcdn.net",
+    ],
+  },
+  env: { NEXTAUTH_URL: nextAuthUrl },
   // pageExtensions: ['page.js']
-}
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
